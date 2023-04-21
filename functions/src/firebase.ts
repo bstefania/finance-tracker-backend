93bc388b37cc3a8af872d54cc2774f9979de61ee
Stefania Bogdan <bogdan_stefania11@yahoo.com>
2023-04-21 18:47:42 +0300

    use json file for firebase credentials

--- functions/src/firebase.ts
+++ functions/src/firebase.ts
@@ -1,18 +1,12 @@
-import admin from 'firebase-admin';
-import { getEnvironmentVariable } from "./utils/EnvironmentVariable"
-import { Firestore } from 'firebase-admin/firestore';
-
+import admin from 'firebase-admin'
+import { Firestore } from 'firebase-admin/firestore'
+import serviceAccount from "../serviceAccountKey.json"
 export let db: Firestore
 
 export const connectToFirebase = () => {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: getEnvironmentVariable("FIREBASE_PROJECT_ID"),
-      privateKey: getEnvironmentVariable("FIREBASE_PRIVATE_KEY").replace(/\\n/g, '\n'),
-      clientEmail: getEnvironmentVariable("FIREBASE_CLIENT_EMAIL")
-    }),
-    storageBucket: getEnvironmentVariable("FIREBASE_STORAGE_BUCKET")
-  });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
+});
   
   db = admin.firestore()
 }
